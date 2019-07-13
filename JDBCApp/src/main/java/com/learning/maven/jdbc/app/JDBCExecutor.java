65bf73a8af6f2ab08a946f88436ccb1afe048d3d
KartikSudarshan <kartiksudd@gmail.com>
2019-07-13 16:21:18 +0530

    Using Stored Procedures

--- JDBCApp/src/main/java/com/learning/maven/jdbc/app/JDBCExecutor.java
+++ JDBCApp/src/main/java/com/learning/maven/jdbc/app/JDBCExecutor.java
@@ -7,50 +7,56 @@ import java.io.IOException;
 import java.sql.Connection;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Properties;
-
 
 public class JDBCExecutor {
 
 	public static void main(String[] args) throws IOException {
-		
-		FileReader propertiesReader=new FileReader("src"+File.separator+"main"+File.separator+"resources"+File.separator+"db.properties");;
-		Properties dbproperties=new Properties();
+
+		FileReader propertiesReader = new FileReader(
+				"src" + File.separator + "main" + File.separator + "resources" + File.separator + "db.properties");
+		;
+		Properties dbproperties = new Properties();
 
 		dbproperties.load(propertiesReader);
 		System.out.println("Checking the connection to the Database");
-		System.out.println("PropertiesFile is "+propertiesReader.toString());
+		System.out.println("PropertiesFile is " + propertiesReader.toString());
 		System.out.println("Hello Learing JDBC");
-		
-		final String host=System.getProperty("database.host", dbproperties.getProperty("database.host"));
-		final String databasename=System.getProperty("postgres.database.name", dbproperties.getProperty("postgres.database.name")); 
-		final String username=System.getProperty("postgres.database.username", dbproperties.getProperty("postgres.database.username")); 
-		final String password=System.getProperty("postgres.database.password", dbproperties.getProperty("postgres.database.password"));
-		
+
+		final String host = System.getProperty("database.host", dbproperties.getProperty("database.host"));
+		final String databasename = System.getProperty("postgres.database.name",
+				dbproperties.getProperty("postgres.database.name"));
+		final String username = System.getProperty("postgres.database.username",
+				dbproperties.getProperty("postgres.database.username"));
+		final String password = System.getProperty("postgres.database.password",
+				dbproperties.getProperty("postgres.database.password"));
+
 		DatabaseConnectionManager dcm = new DatabaseConnectionManager(host, databasename, username, password);
 		System.out.println(dcm.toString());
 		try {
-			 Connection connection =dcm.getConnection();
-			 CustomerDAO customerDAO = new CustomerDAO(connection);
-			/* Creating Customer Values
+			Connection connection = dcm.getConnection();
+			CustomerDAO customerDAO = new CustomerDAO(connection);
+			/*
+			 * Creating Customer Values
 			 * 
 			 * Customer customer = new Customer(); customer.setFirstname("Arthur");
 			 * customer.setLastname("Morgan");
 			 * customer.setEmail("arthur.morgan@red.dead.redemption");
 			 * customer.setPhone("(555) 555-1234"); customer.setAddress("1234 Main St");
 			 * customer.setCity("Mount Vernon"); customer.setState("VA");
-			 * customer.setZipcode("22121");
-			 * customerDAO.create(customer);
+			 * customer.setZipcode("22121"); customerDAO.create(customer);
 			 */
-			 
-			/* Reading Customer Values
+
+			/*
+			 * Reading Customer Values
 			 * 
 			 * Customer customer =customerDAO.findById(197);
 			 * System.out.println(customer.getFirstname()+" "+customer.getLastname());
 			 */
-			 
-			 
-			/* Updating Data 
+
+			/*
+			 * Updating Data
 			 * 
 			 * Customer customer=customerDAO.findById(1000);
 			 * System.out.println(customer.getFirstname()+" "+customer.getLastname()+" "
@@ -61,21 +67,23 @@ public class JDBCExecutor {
 			 * System.out.println(customer.getFirstname()+" "+customer.getLastname()+" "
 			 * +customer.getEmail());
 			 */
-			 
-			  Customer customer = new Customer(); 
-			  customer.setFirstname("John");
-			  customer.setLastname("Marston");
-			  customer.setEmail("john.marston@red.dead.redemption");
-			  customer.setPhone("(555) 555-1234"); 
-			  customer.setAddress("1234 Main St");
-			  customer.setCity("Mount Vernon"); 
-			  customer.setState("VA");
-			  customer.setZipcode("22121");
-			  Customer dbCustomer=customerDAO.create(customer);
-			  System.out.println(dbCustomer);
-			  customerDAO.delete(dbCustomer.getId());
-			 
-		}catch (SQLException e) {
+
+			/*
+			 * Deleting an entry Customer customer = new Customer();
+			 * customer.setFirstname("John"); customer.setLastname("Marston");
+			 * customer.setEmail("john.marston@red.dead.redemption");
+			 * customer.setPhone("(555) 555-1234"); customer.setAddress("1234 Main St");
+			 * customer.setCity("Mount Vernon"); customer.setState("VA");
+			 * customer.setZipcode("22121"); Customer
+			 * dbCustomer=customerDAO.create(customer); System.out.println(dbCustomer);
+			 * customerDAO.delete(dbCustomer.getId());
+			 *
+			 */
+
+			OrderDAO orderDAO = new OrderDAO(connection);
+			List<Order> orders = orderDAO.getOrdersForCustomer(789);
+			orders.forEach(System.out::println);
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
